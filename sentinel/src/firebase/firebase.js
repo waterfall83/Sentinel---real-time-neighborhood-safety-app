e888694e185ccf0fada898774ca208ed624a7299
arie <a.y.liang9@gmail.com>
2025-10-27 22:38:57 -0700

    firebase setup + hidden api key

--- sentinel/src/firebase/firebase.js
+++ sentinel/src/firebase/firebase.js
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "MESSAGING_SENDER_ID",
-    appId: "APP_ID"
+  apiKey: "AIzaSyCI288hHTMfQWAaeXQcia_8InFf9npxRmk",
+  authDomain: "sentinel-10d40.firebaseapp.com",
+  projectId: "sentinel-10d40",
+  storageBucket: "sentinel-10d40.firebasestorage.app",
+  messagingSenderId: "619299607719",
+  appId: "1:619299607719:web:02526ad7cb620321b87782",
+  measurementId: "G-VMP1HBCT4B"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-export default app;
+const analytics = getAnalytics(app);
